1722d3b88affd7801f7d22923a27d6a99c522a9e
lr948435406 <948435406@qq.com>
2017-04-11 16:17:21 +0800

    添加了主页 avtivity_home

---
+++ app/src/main/java/com/zykj/activity/Test.java
@@ -0,0 +1,18 @@
+package com.zykj.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.zykj.yixiuge.R;
+
+/**
+ * Created by zykj on 2017/4/11.
+ */
+
+public class Test extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
